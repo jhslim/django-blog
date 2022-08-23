3ca213a84f1afba903f4b0b5150ad57190700460
jhslim <jlim.uw@gmail.com>
2022-08-22 22:04:50 -0700

    fixing database config

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -3,11 +3,9 @@ import dj_database_url
 from .settings import *
 
 DATABASES = {
-    "default": {
-        "default": dj_database_url.config(
-            default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
-        )
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 DEBUG = False
